cf26168f4b1620eeb1bf2c4088f2d57f80ac9c2b
Chris Morgan <cmorgan@abamaxa.com>
2023-06-03 15:05:16 +0100

    support for progress events

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,14 @@
-import React, {useEffect, useRef, useState} from "react";
-import {createLogger, log_info, log_warning} from "./Logger";
-import {SocketAdaptor} from "./Socket";
-import {RemoteMessage} from "./Messages";
+import React from "react";
+import {createLogger} from "./Logger";
 import {HTTPRestAdaptor, RestAdaptor} from "./RestAdaptor";
+import Viewer from "./Viewer";
 
 const host: RestAdaptor = new HTTPRestAdaptor();
 
 createLogger(host);
 
 const App = () => {
-
-  const [currentVideo, setCurrentVideo] = useState("");
-  const videoControlRef = useRef(null);
-
-  const togglePause = (vc: HTMLVideoElement) => {
-    if (vc.paused) {
-      vc.play().catch(err => {
-        log_warning(err.message);
-      })
-    } else {
-      vc.pause();
-    }
-  };
-
-  useEffect(() => {
-
-    new SocketAdaptor(
-      () => new WebSocket(`ws://${host.getHost()}/remote/ws`),
-      (message: RemoteMessage) => {
-        if (message.Play !== undefined) {
-          log_info(`Got play message: ${message.Play.url}`);
-
-          let url: string;
-          if (message.Play.url.startsWith("http")) {
-            // hacky fix for samsung tvs
-            url = message.Play.url.replace("\/stream\/", "/alt-stream/");
-          } else {
-            url = `http://${host.getHost()}${message.Play.url}`;
-          }
-          setCurrentVideo(url);
-
-        } else if (message.Seek !== undefined && videoControlRef !== null) {
-          const vc = videoControlRef.current as unknown as HTMLVideoElement;
-          vc.currentTime = vc.currentTime + message.Seek.interval;
-
-        } else if (message.TogglePause !== undefined && videoControlRef !== null) {
-          const vc = videoControlRef.current as unknown as HTMLVideoElement;
-          togglePause(vc);
-        } else {
-          log_warning(`Got unknown message: ${message}`);
-        }
-      }
-    );
-
-  }, [])
-
-  const getNextVideo = (_: React.SyntheticEvent<HTMLVideoElement>) => {
-    log_info("getNextVideo called");
-  }
-
-  const logVideoError = (e: React.SyntheticEvent<HTMLVideoElement>) => {
-    const error = (e.target as HTMLVideoElement).error;
-    if (error) {
-      log_info(`Video error: ${error.message}`);
-    }
-  }
-
-  if (currentVideo !== "") {
-    return (
-      <div className="bg-black h-screen w-screen">
-        <video
-          className="h-screen m-auto"
-          onEnded={e => getNextVideo(e)}
-          onClick={e => togglePause(e.currentTarget)}
-          onError={e => logVideoError(e)}
-          style={{objectFit: "contain"}}
-          id="video"
-          autoPlay={true}
-          controls
-          muted={false}
-          playsInline={false}
-          src={currentVideo}
-          ref={videoControlRef}
-        >
-        </video>
-      </div>
-    )
-  } else {
-    return (
-      <h1 className="text-6xl text-white bg-black text-center h-screen py-32">
-        Waiting for video to be selected
-      </h1>
-    )
-  }
+  return (<Viewer host={host}/>);
 };
 
 export default App;
--- src/Socket.tsx
+++ src/Socket.tsx
@@ -1,80 +1,187 @@
-import {log_info} from "./Logger";
+/**
+ * Contains functions for communicating with a remote server and handling messages received via WebSocket.
+ */
+import {log_info, log_error} from "./Logger";
 import {string} from "prop-types";
 import {RemoteMessage} from "./Messages";
 
-
+/**
+ * The type of function callback that executes when a message is received from the server.
+ */
 type onMessageCallback = ((message: RemoteMessage) => void);
 
+/**
+ * A singleton class that acts as an Adaptor for a WebSocket that
+ * communicates with a remote server.
+ */
 export class SocketAdaptor {
+  /** The single instance of SocketAdaptor */
   private static _instance: SocketAdaptor;
+  /** The function that creates and returns a WebSocket */
   private static socketBuilder: (() => WebSocket) | undefined;
+  /** The WebSocket instance being managed */
   private socket?: WebSocket = undefined;
+  /** The flag indicating whether the WebSocket connection is open */
   private open: boolean = false;
+  /** The flag indicating whether WebSocket message listeners have been added */
   private listening: boolean = false;
+  /** The callback that executes when a message is received from the server */
   private readonly onMessage?: onMessageCallback = undefined;
 
+  /**
+   * Constructs a new instance of SocketAdaptor.
+   * @param socketBuilder - A function that creates and returns a WebSocket.
+   * @param onMessage - A callback that executes when a message is received from the server.
+   */
   constructor(socketBuilder: () => WebSocket, onMessage: onMessageCallback) {
+    // Ensure only one instance exists.
     if (SocketAdaptor._instance) {
       return SocketAdaptor._instance;
     }
     SocketAdaptor._instance = this;
-    SocketAdaptor.socketBuilder = socketBuilder;
 
+    // Initialize properties.
+    SocketAdaptor.socketBuilder = socketBuilder;
     this.onMessage = onMessage;
     this.socket = socketBuilder();
     this.addListeners();
   }
 
+  /**
+   * Adds listeners for WebSocket open and message events.
+   */
   addListeners = () => {
     if (this.socket !== undefined && !this.listening) {
-
       this.listening = true;
       const self = this;
 
-      this.socket.addEventListener('open', (_) => {
+      // Sends a message when the WebSocket is opened.
+      this.socket.onopen = () => {
         console.log("open websocket event");
         if (self.socket !== undefined) {
-          self.socket.send('Hello Server!');
+          self.open = true;
+          self.send({SendLastState: null});
         }
+      };
 
-        self.open = true;
-      });
+      this.socket.onerror = (error) => {
+        console.log(`WebSocket encountered error: ${JSON.stringify(error)}, closing socket`);
+        setTimeout(self._reconnect, 1000);
+      };
 
-      this.socket.addEventListener('message', function (event) {
+      // Parses a message when one is received.
+      this.socket.onmessage = (event) => {
         self.onReceive(event).catch(err => log_info(`onReceive exception: ${err}`));
-      });
+      }
     }
   }
 
+  /**
+   * Closes the WebSocket connection.
+   * @param code - A numeric value representing the code for closing the connection.
+   * @param reason - A reason for closing the connection.
+   */
   close = (code?: number | undefined, reason?: string | undefined) => {
     if (this.socket !== undefined) {
-      this.socket.close(code, reason);
+      try {
+        this.socket.close(code, reason);
+      } catch {
+        // ignore any errors from attempting to close the socket.
+      }
+
       this.socket = undefined;
       this.open = false;
       this.listening = false;
     }
   }
 
-  send = (message: string | Blob) => {
+  /**
+   * Sends a string to the remote server.
+   * @param message - The message to send.
+   */
+  send_string = (message: string) => {
     if (this.socket !== undefined && this.open) {
       this.socket.send(message);
     }
   }
 
-  onReceive = async (event: any) => {
-    if (this.socket !== undefined && this.open && this.onMessage !== undefined) {
-      if (event.data instanceof string) {
-        log_info(event.data);
-      } else {
-        const txt = await new Response(event.data).text();
-        log_info(`onReceive: ${txt}`);
-        let data = JSON.parse(txt);
-        this.onMessage(data);
+  /**
+   * Sends a message to the remote server.
+   * @param message - The message to send.
+   */
+  send = <T,>(message: T) => {
+    if (this.socket !== undefined && this.open) {
+      // Serialize the object as a JSON string
+      const jsonString = JSON.stringify(message);
+
+      // Convert the JSON string to a Blob
+      const blob = new Blob([jsonString], { type: "application/json" });
+
+      if (this.socket.readyState > 1) {
+        this._reconnect();
+      }
+
+      try {
+        this.socket.send(blob);
+      } catch (error) {
+        this._reconnect();
+        this.socket.send(blob);
       }
     }
   }
 
+  /**
+   * Parses a message received from the server.
+   * @param event - The event containing the message.
+   */
+  onReceive = async (event: any) => {
+    if (this.socket !== undefined && this.open && this.onMessage !== undefined) {
+      if (event.data instanceof string) {
+        // Log message.
+        log_info(event.data);
+      } else {
+        // Parse received message.
+        await this._parseMessage(event);
+      }
+    }
+  }
+
+  /**
+   * Parses a message received from the server.
+   * @param event - The event to parse.
+   */
+  _parseMessage = async (event: any)=> {
+    try {
+      // Convert response to text.
+      const txt = await new Response(event.data).text();
+      // Parse JSON data.
+      let data = JSON.parse(txt);
+      // Execute callback with parsed data.
+      this.onMessage!(data);
+    } catch (error) {
+      // Log and throw error.
+      log_error(`error ${error}, received unexpected message: ${event.data}`);
+      //throw error;
+    }
+  }
+
+  /**
+   * Returns true if the WebSocket is open and ready for communication.
+   * @return - A boolean indicating if the WebSocket is open.
+   */
   isReady = ():boolean => {
     return (this.socket !== undefined && this.open);
+  }
+
+  _reconnect = () => {
+    if (SocketAdaptor.socketBuilder) {
+      try {
+        this.close();
+        this.socket = SocketAdaptor.socketBuilder();
+        this.addListeners();
+      } catch (error) {
+        console.log(`reconnecting websocket: ${error}`);
+      }
+    }
   }
 }
